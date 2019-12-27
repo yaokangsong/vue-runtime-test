e20710fe7c490ae85ed9ea61a6a52c68391ff504
maxyao <maxyao@lexin.com>
2019-12-27 18:34:11 +0800

    dfsd

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <div>数据量: {{ this.list.length }}</div>
+    <div v-for="(d, i) in list" @click="click(i)">
+      <span>
+        <span> 
+          <span>
+            <span>
+              Hello {{name}} {{i}} {{d}}!
+            </span>
+          </span>
+        </span>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      name: 'vue',
+      list: new Array(500)
+    }
+  },
+  methods: {
+    click(index){
+      this.list.splice(index, 1, 'test')
+    }
+  },
+}
+</script>
+
+
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // 选项...
+    filenameHashing: false,
+    publicPath: 'D:/Users/Maxyao/Desktop/learn-demo/vue-runtime-test/dist'
+  }
